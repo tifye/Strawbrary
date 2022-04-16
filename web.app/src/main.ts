e48d1f9488e6e1c1929979b4067d3d3eb39f0ec4
tifufu <dematasjoshua@hotmail.com>
2022-04-16 21:24:45 +0200

    App now uses helmet package for extra security

--- web.app/src/main.ts
+++ web.app/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './.pipes/validation.pipe';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
