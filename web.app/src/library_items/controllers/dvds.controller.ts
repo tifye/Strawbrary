07a0a456415a6a7ecff2bfc46c5f8b04fdb8d73b
tifufu <dematasjoshua@hotmail.com>
2022-04-18 21:51:24 +0200

    Created a createDvdDto class

--- web.app/src/library_items/controllers/dvds.controller.ts
+++ web.app/src/library_items/controllers/dvds.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateDvdDto } from '../dto/create_dvd.dto';
 
 @Controller('dvds')
 export class DvdsController {
   @Post()
-  async create() {
+  async create(@Body() createDvdDto: CreateDvdDto) {
     return {
       message: 'Hello World',
     };
