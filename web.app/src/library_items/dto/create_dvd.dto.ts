ce60cbcd0d7237e061b20024374193c92498a703
tifufu <dematasjoshua@hotmail.com>
2022-04-22 01:30:48 +0200

    Fixed bug on all dto where one could update or add fields that were not intended

--- web.app/src/library_items/dto/create_dvd.dto.ts
+++ web.app/src/library_items/dto/create_dvd.dto.ts
@@ -1,14 +1,18 @@
+import { Expose } from 'class-transformer';
 import { IsNumber, IsString, Length, Min } from 'class-validator';
 
 export class CreateDvdDto {
+  @Expose()
   @IsString()
   @Length(4, 255)
   title: string;
 
+  @Expose()
   @IsNumber()
   @Min(4)
   runTimeMinutes: number;
 
+  @Expose()
   @IsNumber()
   @Min(0)
   categoryId: number;
--- web.app/src/library_items/dto/update_book.dto.ts
+++ web.app/src/library_items/dto/update_book.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Expose } from 'class-transformer';
 import {
   IsBoolean,
   IsDate,
@@ -10,18 +11,22 @@ import { ValidItemTypeRule } from '../rules/valid_item_type.rule';
 import { CreateBookDto } from './create_book.dto';
 
 export class UpdateBookDto extends PartialType(CreateBookDto) {
+  @Expose()
   @IsBoolean()
   @IsOptional()
   isBorrowable?: boolean;
 
+  @Expose()
   @IsDate()
   @IsOptional()
   borrowDate?: Date;
 
+  @Expose()
   @IsString()
   @IsOptional()
   borrower?: string;
 
+  @Expose()
   @IsString()
   @IsOptional()
   @Validate(ValidItemTypeRule)
