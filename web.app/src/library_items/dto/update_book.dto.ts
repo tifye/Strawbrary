18bb5f49b17e8f2786d607110b3d9d89f36bcd26
tifufu <dematasjoshua@hotmail.com>
2022-04-22 00:39:30 +0200

    Updated update book functionality to also be able to update the item type. Wrote a unit test for book service update

--- web.app/src/library_items/dto/update_book.dto.ts
+++ web.app/src/library_items/dto/update_book.dto.ts
@@ -1,5 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsBoolean, IsDate, IsOptional, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsDate,
+  IsOptional,
+  IsString,
+  Validate,
+} from 'class-validator';
+import { ValidItemTypeRule } from '../rules/valid_item_type.rule';
 import { CreateBookDto } from './create_book.dto';
 
 export class UpdateBookDto extends PartialType(CreateBookDto) {
@@ -14,4 +21,9 @@ export class UpdateBookDto extends PartialType(CreateBookDto) {
   @IsString()
   @IsOptional()
   borrower?: string;
+
+  @IsString()
+  @IsOptional()
+  @Validate(ValidItemTypeRule)
+  type?: string;
 }
