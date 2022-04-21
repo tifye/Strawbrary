007527a76fd9c4e761cf31cbe28283a4817e1c7c
tifufu <dematasjoshua@hotmail.com>
2022-04-22 01:29:09 +0200

    Can now update reference books

--- web.app/src/library_items/dto/update_reference_book.dto.ts
+++ web.app/src/library_items/dto/update_reference_book.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Expose } from 'class-transformer';
 import { IsString, IsOptional, Validate } from 'class-validator';
 import { ValidItemTypeRule } from '../rules/valid_item_type.rule';
 import { CreateReferenceBookDto } from './create_reference_book.dto';
@@ -6,6 +7,7 @@ import { CreateReferenceBookDto } from './create_reference_book.dto';
 export class UpdateReferenceBookDto extends PartialType(
   CreateReferenceBookDto,
 ) {
+  @Expose()
   @IsString()
   @IsOptional()
   @Validate(ValidItemTypeRule)
