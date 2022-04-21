958c1ca806d8f5f59edf9669dda77866a64145bf
tifufu <dematasjoshua@hotmail.com>
2022-04-22 01:51:39 +0200

    Remoed borrow realted data from update book dto. Goal is to create specific routes to check in/out items

--- web.app/src/library_items/dto/update_book.dto.ts
+++ web.app/src/library_items/dto/update_book.dto.ts
@@ -1,31 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { Expose } from 'class-transformer';
-import {
-  IsBoolean,
-  IsDate,
-  IsOptional,
-  IsString,
-  Validate,
-} from 'class-validator';
+import { IsOptional, IsString, Validate } from 'class-validator';
 import { ValidItemTypeRule } from '../rules/valid_item_type.rule';
 import { CreateBookDto } from './create_book.dto';
 
 export class UpdateBookDto extends PartialType(CreateBookDto) {
-  @Expose()
-  @IsBoolean()
-  @IsOptional()
-  isBorrowable?: boolean;
-
-  @Expose()
-  @IsDate()
-  @IsOptional()
-  borrowDate?: Date;
-
-  @Expose()
-  @IsString()
-  @IsOptional()
-  borrower?: string;
-
   @Expose()
   @IsString()
   @IsOptional()
