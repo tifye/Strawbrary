6388cf2ea6378a5c426315a30cdbe0b4d692c3a7
tifufu <dematasjoshua@hotmail.com>
2022-04-24 21:16:34 +0200

    separated search field into own file

--- react.app/src/components/pages/library_items/LibraryItemsContentAppBar.tsx
+++ react.app/src/components/pages/library_items/LibraryItemsContentAppBar.tsx
@@ -1,54 +1,11 @@
 import {
   AppBar,
-  InputBase,
-  styled,
   Toolbar,
   Typography,
 } from '@mui/material';
 import { Search } from '@mui/icons-material';
 import React from 'react';
-
-const SearchField = styled('div')(({ theme }) => ({
-  position: 'relative',
-  color: 'black',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: 'white',
-  marginLeft: 0,
-  marginRight: theme.spacing(2),
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  backgroundColor: 'transparent',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
+import { SearchField, SearchIconWrapper, StyledInputBase } from '../../sub_components/SearchField';
 
 export default function LibraryItemsContentAppBar() {
   const [search, setSearch] = React.useState('');
