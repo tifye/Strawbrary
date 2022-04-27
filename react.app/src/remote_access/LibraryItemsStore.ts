451398e3c5bc8ec1512b0a08b600f4dc1f7b707e
tifufu <dematasjoshua@hotmail.com>
2022-04-27 23:12:55 +0200

    Can now change items type while updating other information in frontend

--- react.app/src/remote_access/LibraryItemsStore.ts
+++ react.app/src/remote_access/LibraryItemsStore.ts
@@ -45,7 +45,9 @@ export class LibraryItemsStore {
   public async updateLibraryItem(item: LibraryItem): Promise<boolean> {
     const typePath = (typePaths as any)[item.type];
     try {
-      const response = await this.axios.patch(`${url}/${typePath}/${item.id}`, item);
+      const changeTypeResponse = await this.axios.post(`${url}/items/${item.id}/type`, { type: item.type });
+      const updateItemTypeItem = plainToInstance(LibraryItem, changeTypeResponse.data);
+      const response = await this.axios.patch(`${url}/${typePath}/${item.id}`, updateItemTypeItem);
       return Boolean(response.data.wasSuccessful);
     } catch (error: any) {
       if (this.axios.isAxiosError(error) && error.response) {
